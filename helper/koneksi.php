7ef74b30d526c2a8a99b04fb52d0eb2f3351bb19
mohammedh8 <92924526+mohammedh8@users.noreply.github.com>
2021-10-22 16:08:21 +0300

    Update koneksi.php

--- helper/koneksi.php
+++ helper/koneksi.php
@@ -5,7 +5,7 @@ $username = "epiz_29938962";
 $password = "5NKIUCFwimtVHgt";
 $db = "epiz_29938962_wagate";
 
-$koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
+$koneksi = mysqli_connect($host, $username, $password, $db) or die("toto");
 
 $base_url = "http://localhost/wa/";
 date_default_timezone_set('Asia/Jakarta');
