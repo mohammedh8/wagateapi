0cd21d4034594efa87c04fdf5cbaa5d59243fcbf
mohammedh8 <92924526+mohammedh8@users.noreply.github.com>
2021-10-22 15:29:42 +0300

    Update koneksi.php

--- helper/koneksi.php
+++ helper/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$db = "wa";
+$host = "dev-damas.com";
+$username = "damasbot";
+$password = "Mm5329924";
+$db = "wagate";
 
 $koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
 
